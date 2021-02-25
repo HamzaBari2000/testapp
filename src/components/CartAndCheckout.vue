c2c3e581e1a65af164eafcbb001d8dff9392df56
HamzaBari2000 <67334675+HamzaBari2000@users.noreply.github.com>
2021-02-25 18:09:22 +0000

    commit 3

--- src/components/CartAndCheckout.vue
+++ src/components/CartAndCheckout.vue
@@ -38,7 +38,11 @@
             <p>Phone Number: {{Phonenumber}}</p>
             <!--If the users inputted name and phoneNo is both true it will display the checkout button else it won't-->
             <!--Once it's true then, the user can purchase-->
-            <button>Checkout</button>
+            <button @click="purchase()" v-if="checkCheckout">Checkout</button>
+            <p v-show="checkoutMsg" id="msg">
+                Your Order has been purhcased.
+                Wait 5 Seconds as you are being redirected...
+            </p>
         </div>
     </div>
 
@@ -52,12 +56,33 @@ export default {
         return {
             name: "",
             Phonenumber: "",
+            checkoutMsg: false,
         };
     },
     methods: {
         remove(lesson) {
             this.$emit("removeFromCart", lesson);
+        },
+        validateCheckout(name, Phonenumber) {   //This function is used to validate the name and the phonenumber.
+            let regExname = /^[A-Za-z]+$/;
+            let regExphoneNumber = /^[\s()+-]*([0-9][\s()+-]*){11,20}$/;
+            return regExname.test(name) && regExphoneNumber.test(Phonenumber);  //It will check the name and phonenumber with each of their regex.
+        },
+        purchase() {
+            this.checkoutMsg = true;
+             setTimeout(function () {
+                location.reload();
+            }, 5000);   //Refreshes after 5 seconds. 
         }
     },
+    computed: {
+        checkCheckout() { //This function checks the validation of the name and phonenumber inputted by the validate function.
+            if (!this.validateCheckout(this.name, this.Phonenumber)) {
+                return false;   //Returns false which means either the name, phonoNo, or Both are incorrect.
+            } else {
+                return true;    //Returns true if both matches the regex. 
+            }
+        },
+    }
 };
 </script>
